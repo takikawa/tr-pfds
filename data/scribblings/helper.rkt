6c5dc63e9f0d38af1e8d6bd56c6c463f91594a4c
Mike Sperber <sperber@deinprogramm.de>
2013-01-22 10:39:58 +0100

    Resolve collection inconsistencies.
    - move the scribblings so that they build again
    - minor fixes to the scribblings build
    - update module references in README

---
+++ data/scribblings/helper.rkt
@@ -0,0 +1,17 @@
+#lang at-exp racket/base
+(require scribble/manual
+         (for-label racket/base)
+	 scribble/eval)
+
+(define racket-map @racket[map])
+(define racket-foldl @racket[foldl])
+(define racket-foldr @racket[foldr])
+(define racket-filter @racket[filter])
+(define racket-remove @racket[remove])
+(define racket-andmap @racket[andmap])
+(define racket-ormap @racket[ormap])
+(define racket-build-list @racket[build-list])
+(define racket-make-list @racket[make-list])
+(provide racket-map racket-foldl racket-foldr racket-filter racket-remove
+         racket-andmap racket-ormap racket-build-list racket-make-list
+	 make-base-eval examples close-eval)
