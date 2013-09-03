af888e91be17e6b6cb2d1490b46663b92b6dbd15
Asumu Takikawa <asumu@ccs.neu.edu>
2013-09-03 14:09:31 -0400

    Update pkg deps

--- info.rkt
+++ info.rkt
@@ -2,6 +2,6 @@
 
 ;; Planet 2
 (define collection 'multi)
-(define deps '("base" "typed-racket-lib"))
+(define deps '("base" "typed-racket-lib" "scheme-lib"))
 (define build-deps '("at-exp-lib" "htdp-lib" "racket-doc" "scribble-lib" "typed-racket-more"))
 
